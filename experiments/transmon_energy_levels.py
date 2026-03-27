fafa587a9fd44466a00802b214583ed049b73612
malinabochis <malina.bochis@bestcj.ro>
2026-03-27 05:23:12 +0200

    wrote function for getting the eigenenergies of transmon qubit hamiltonian
    plotted the eigenenergies as a function of the offset charge for different ratios EJ/EC

---
+++ experiments/transmon_energy_levels.py
@@ -0,0 +1,104 @@
+# import numpy
+# import numpy as np
+#
+#
+def k_sorting(n, ng):
+    """
+
+    :param n: qubit energy level index
+    :param ng: offset / gate charge
+    :return:
+    """
+    term1 = -np.round(ng) # round(x) is the nearest integer to the real number x, it changes value at half whole numbers => Brillouin zone boundary
+    # floor(x + 0.5) is used to solve the round(x) problem that it rounds numbers to nearest even number
+    term2 = 0.25 * ( (-1) ** (np.floor(ng)) ) # floor(x) is the integer immediately below x, it changes value only at whole numbers
+    term3 = -1 + ( (-1) ** n) * (1 + 2*n)
+
+    return term1 + term2 * term3
+
+#
+# energy_raport = E_j / E_c
+# q = - 0.5 * energy_raport
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import mathieu_a
+
+
+def get_transmon_energies_mathieu(n, ng, EJ_EC_ratio):
+    """
+    Calculeaza energia nivelului n conform Eq. 2.2 din Koch et al.
+    E_C este setat conventional la 1 pentru calculul raportului.
+    """
+    EC = 1.0
+    # EJ = EJ_EC_ratio * EC
+    q = - 0.5 * EJ_EC_ratio
+
+    # Order v for mathieu characteristic value a_v
+    v = 2 * (ng + k_sorting(n, ng))
+    return EC * mathieu_a(v, q) # returns energy scaled about chosen unit (EC)
+    # mathieu_a(level to be calculated, potential form)
+    #a_v(q) = functia a de ordin v evaluata in q
+
+
+def get_transmon_energies(n_states_to_return, ng, EJ_EC_ratio):
+    """
+    Diagonalizarea directa a Hamiltonianului (Eq. 2.1)
+    """
+    EC = 1.0
+    EJ = EJ_EC_ratio * EC
+
+    # Definim baza de stari de sarcina (de la -15 la 15 e suficient)
+    n_basis = np.arange(-15, 16)
+    size = len(n_basis)
+    H = np.zeros((size, size))
+
+    # Umplem diagonala cu energia cinetica: 4*Ec*(n - ng)^2
+    for i in range(size):
+        H[i, i] = 4 * EC * (n_basis[i] - ng) ** 2
+
+    # Umplem off-diagonala cu energia Josephson: -Ej/2 (hopping)
+    for i in range(size - 1):
+        H[i, i + 1] = -EJ / 2.0
+        H[i + 1, i] = -EJ / 2.0
+
+    # Calculam valorile proprii (energiile)
+    energies = np.linalg.eigvalsh(H)
+    return energies[:n_states_to_return]  # Returnam primele n states (0, 1, 2)
+
+
+ng_axis = np.linspace(-2, 2, 500) # 500 experimental points
+ratios = [1.0, 5.0, 10.0, 50.0]
+levels = [0, 1, 2]
+colors = ['black', 'red', 'blue']
+
+fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+axes = axes.flatten()
+
+n_states_to_return = 3
+
+for i, ratio in enumerate(ratios): # i is the graphic index
+    # normalization factor E01 at ng = 0.5
+    energies_ss = get_transmon_energies(n_states_to_return, 0.5, ratio) # energy 0 at sweet spot
+    E01_ref = energies_ss[1] - energies_ss[0]
+
+    all_energies = np.array([get_transmon_energies(n_states_to_return, ng, ratio) for ng in ng_axis])
+    e0_min = np.min(all_energies[:, 0]) # minimum of ground state energy, take only first column from all rows for E0
+    # will set the zero point energy as the bottom of m = 0 level
+
+    for m in levels:
+        energies_raw = all_energies[:, m]
+        # Normalization: (E - min(E0)) / E01_ref
+        energies_norm = (energies_raw - e0_min) / E01_ref
+        axes[i].plot(ng_axis, energies_norm, color=colors[m], label=f'm={m}')
+        # i is the graphic index
+
+    # Formatare grafic conform originalului
+    axes[i].set_title(f'EJ/EC = {ratio}')
+    axes[i].set_xlabel('$n_g$')
+    axes[i].set_ylabel('$E_m / E_{01}$')
+    axes[i].set_ylim(-0.5, 10 if ratio == 1.0 else 3)  # adjusting Y scale
+    axes[i].grid(True, linestyle='--', alpha=0.6)
+
+plt.tight_layout()
+plt.show()
